330f328acd79bfbb3f27a431dd3aa3d7194df466
tlemarec <titouan-lemarec@outlook.com>
2018-04-09 23:29:23 +0200

    Restored Spi speed

--- SPI.cpp
+++ SPI.cpp
@@ -31,7 +31,7 @@ int SpiOpenPort (int spi_device)
 
 
 	if (spi_device)
-		*spi_cs_fd = open(std::string("/dev/spidev0.1").c_str(), O_RDWR);
+		*spi_cs_fd = open(std::string("/dev/spidev0.0").c_str(), O_RDWR);
 	else
 		*spi_cs_fd = open(std::string("/dev/spidev0.0").c_str(), O_RDWR);
 
